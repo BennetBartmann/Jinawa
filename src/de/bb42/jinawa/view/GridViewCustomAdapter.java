19365db85e0c7aeaa6cc55b64a393607ddfb2490
Bennet Bartmann <bennet.bartmann@googlemail.com>
2014-10-13 10:19:40 +0200

    Added JDocs for GridViewAdapter
    Added a few JDocs for the Grid

--- src/de/bb42/jinawa/view/GridViewCustomAdapter.java
+++ src/de/bb42/jinawa/view/GridViewCustomAdapter.java
@@ -1,7 +1,6 @@
 package de.bb42.jinawa.view;
 
 import java.util.List;
-
 import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
@@ -13,23 +12,41 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import de.bb42.jinawa.R;
 
+/**
+ * Custom View Adapter for the Grid
+ * @author Johannes Becker
+ *
+ */
 public class GridViewCustomAdapter extends ArrayAdapter<Object> {
 	Context context;
 	List<String> names;
 	int res;
 	int i = 0;
-
+	/**
+	 * Constructor of Custom view Adapter
+	 * @param context Context of the Android Application
+	 * @param names 
+	 * @param res 
+	 */
 	public GridViewCustomAdapter(Context context, List<String> names, int res) {
 		super(context, 0);
 		this.context = context;
 		this.names = names;
 		this.res = res;
 	}
-
+	/**
+	 * @return size of the Grid
+	 */
 	public int getCount() {
 		return names.size();
 	}
 
+	/**
+	 * @param position
+	 * @param convertView
+	 * @param parent
+	 * @return View the View
+	 */
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View row = convertView;
 		if (row == null) {
