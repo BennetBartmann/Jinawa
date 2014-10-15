5400c0f89f7c66210d77150df9518ecfe95ee397
Bennet Bartmann <bennet.bartmann@googlemail.com>
2014-10-15 19:18:37 +0200

    Corrected API Level to 8
    But have no Idea if this is working...

--- src/de/bb42/jinawa/modell/Utils.java
+++ src/de/bb42/jinawa/modell/Utils.java
@@ -2,34 +2,46 @@ package de.bb42.jinawa.modell;
 
 import java.io.File;
 
+import android.annotation.SuppressLint;
 import android.os.Environment;
 
 /**
  * Static Class for all util Methods regarding storage
+ * 
  * @author Bennet Bartmann
  * 
  */
 public class Utils {
 	/**
-	 *  Checks if external storage is available for read and write 
+	 * Checks if external storage is available for read and write
 	 */
 	public boolean isExternalStorageWritable() {
-	    String state = Environment.getExternalStorageState();
-	    if (Environment.MEDIA_MOUNTED.equals(state)) {
-	        return true;
-	    }
-	    return false;
+		String state = Environment.getExternalStorageState();
+		if (Environment.MEDIA_MOUNTED.equals(state)) {
+			return true;
+		}
+		return false;
 	}
-	
+
 	/**
 	 * Create a Jinawa Directory in the Documents section
+	 * 
 	 * @return the file Pointer
 	 */
-	public File getAlbumStorageDir() {
-	    File file = new File(Environment.getExternalStoragePublicDirectory(
-	            Environment.DIRECTORY_DOCUMENTS), "Jinawa");
-	    if (!file.mkdirs()) {
-	    }
-	    return file;
+	@SuppressLint("NewApi") public File getAlbumStorageDir() {
+		int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+		File file;
+		if (currentapiVersion > 18) {
+			file = new File(
+					Environment
+							.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS),
+					"Jinawa");
+			if (!file.mkdirs()) {
+			}
+		} else {
+			file = new File(Environment.getExternalStorageDirectory()
+					+ "Jinawa");
+		}
+		return file;
 	}
 }
