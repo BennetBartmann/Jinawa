62f18e1ef52d0edd37ab716f16a0e845b0e755aa
Bennet Bartmann <bennet.bartmann@googlemail.com>
2014-10-31 14:05:48 +0100

    Last Final Fix

--- src/de/bb42/jinawa/view/SlideScreenPapers.java
+++ src/de/bb42/jinawa/view/SlideScreenPapers.java
@@ -7,7 +7,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.content.IntentCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
