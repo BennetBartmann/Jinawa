e21c94026f432fc1ac7f3acc65a4ffde19ee6e8d
Trve92 <trve92@gmail.com>
2014-12-07 22:37:59 +0100

    Cleanup 2
    deleted some layout

--- gen/de/bb42/jinawa/R.java
+++ gen/de/bb42/jinawa/R.java
@@ -27,24 +27,20 @@ public final class R {
         public static final int staple=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int button1=0x7f080003;
-        public static final int deletePaperButton=0x7f080004;
-        public static final int deleteStapleButton=0x7f080005;
-        public static final int editText=0x7f080001;
-        public static final int horizontalScrollView1=0x7f080002;
-        public static final int renameStapleButton=0x7f080006;
-        public static final int rl=0x7f080000;
-        public static final int settingsAbout=0x7f080007;
+        public static final int action_settings=0x7f080006;
+        public static final int deletePaperButton=0x7f080002;
+        public static final int deleteStapleButton=0x7f080003;
+        public static final int editText=0x7f080000;
+        public static final int horizontalScrollView1=0x7f080001;
+        public static final int renameStapleButton=0x7f080004;
+        public static final int settingsAbout=0x7f080005;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_paper=0x7f030001;
-        public static final int activity_screen_slide=0x7f030002;
-        public static final int fragment_screen_slide_page=0x7f030003;
-        public static final int longclickdialogpaper=0x7f030004;
-        public static final int longclickdialogstaple=0x7f030005;
-        public static final int settings=0x7f030006;
+        public static final int activity_paper=0x7f030000;
+        public static final int activity_screen_slide=0x7f030001;
+        public static final int longclickdialogpaper=0x7f030002;
+        public static final int longclickdialogstaple=0x7f030003;
+        public static final int settings=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
