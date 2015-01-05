33a5c6446b27f3518f4e60f4696719f30a39f69d
Bennet Bartmann <bennet.bartmann@googlemail.com>
2015-01-05 12:57:59 +0100

    Removing bin to clean up Repository

--- bin/classes/acceptStorys.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$fileName = $_INPUT['name'];
-	$fp = false;
-	while (! ($fp = fopen($fileName, 'w'))){
-		$fileName = $fileName + 1;
-	}
-	fwrite($fp, $_INPUT['content']);
-	fclose($fp);
-?>
